9d44f1c0dc6dd06de7319cc4764bdb8680b4c0cf
NoelElva <93029969+NoelElva@users.noreply.github.com>
2024-01-15 17:38:01 +0530

    Update bitcointX.py
    fixed issue where the crawler would stop due to empty list now it will just recall the function and keep of trying until it get a valid list

--- bitcointX.py
+++ bitcointX.py
@@ -7,9 +7,34 @@ import time
 #urllib.error for the exceptions raised.
 from urllib.error import HTTPError
 C=0 # Condition variable.
+
 A=0 #increment counter.
+
+def txfind ():
+    global A
+    global C
+    r = requests.get(url) 
+    soup = BeautifulSoup(r.content, 'html.parser')  
+    # stores the value of the div with same name and store it in a form of a list
+    article_soup = [result.get_text(separator="<div>", strip=True) for result in soup.find_all( 'div', {'class': 'sc-3391354d-2 gOhep'})] 
+    print(article_soup)
+    #stores the div tag at the 7 index postion 
+    if article_soup == [] :
+        txfind()
+        print("empty list found")
+    else:
+        rep=article_soup[7]
+        if rep != "0.00 BTC"  :
+            print ( "first block is :  "+ B )
+            
+            C=1
+            
+        else:
+            print (A)
+            A =  A+1    
+
 while C == 0: #loops while the c is equal to zero.
-    time.sleep(2.4) #used to add delay.
+    time.sleep(1) #used to add delay.
     B=str(A) #stores A variable in string format.
    
     url="https://www.blockchain.com/explorer/blocks/btc/"+B #URL of the crawling website +B to add the string at the end 
@@ -19,19 +44,6 @@ while C == 0: #loops while the c is equal to zero.
     except HTTPError as hp:
         print(hp)
     else: 
-        r = requests.get(url) 
-        soup = BeautifulSoup(r.content, 'html.parser')  
-        # stores the value of the div with same name and store it in a form of a list
-        article_soup = [result.get_text(separator="<div>", strip=True) for result in soup.find_all( 'div', {'class': 'sc-3391354d-2 gOhep'})] 
-        print(article_soup)
-        #stores the div tag at the 7 index postion 
-        rep=article_soup[7]
-        if rep != "0.00 BTC"  :
-            print ( "first block is :  "+ B )
-        
-            C=1
-        
-        else:
-            print (A)
-            A= A+1
+        txfind ()
+
     
